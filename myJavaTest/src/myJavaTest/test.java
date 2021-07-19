f152d9888fbc23a2b0763229bafc96e53e82bdb0
rabin <rabin@DESKTOP-GJERD8D>
2021-07-19 12:54:35 +0530

    commit and push

---
+++ myJavaTest/src/myJavaTest/test.java
@@ -0,0 +1,12 @@
+package myJavaTest;
+
+public class test {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("New Test class in myJavaTest");
+	}
+
+}
